85ddcdfb0d9351c0f9fc5c2cea0115861d58d08c
Kevin Paul <k.m.paul68@gmail.com>
2025-10-03 21:26:27 -0400

    add second instance example with user_data

--- main.tf
+++ main.tf
@@ -3,6 +3,40 @@ resource "aws_instance" "first_instance" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "first-instance"
+    Name      = "first-instance"
+    terraform = true
   }
+}
+
+resource "aws_instance" "second_instance_with_user_data" {
+  ami             = data.aws_ami.ubuntu_22.id
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.sg_second_instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.xhtml
+              nohup busybox http -f -p 8080 &
+              EOF
+
+  # if user_data changes, replace the instance
+  user_data_replace_on_change = true
+
+  tags = {
+    Name      = "second-instance"
+    terraform = true
+  }
+
+}
+
+resource "aws_security_group" "sg_second_instance" {
+  name = "sg_for_second_instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
