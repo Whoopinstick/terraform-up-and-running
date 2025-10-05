a53b315a8245bb46d4013b4e2fbd4c5781bd641e
Kevin Paul <k.m.paul68@gmail.com>
2025-10-05 16:19:37 -0400

    add asg with lb example

--- datasources.tf
+++ datasources.tf
@@ -13,3 +13,14 @@ data "aws_ami" "ubuntu_22" {
 
   owners = ["099720109477"] # Canonical
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_ip_second_instance_with_user_data" {
   value       = aws_instance.second_instance_with_user_data.public_ip
   description = "The public IP of the second example instance"
 }
+
+output "alb_dns_name" {
+  value       = aws_lb.lb_third_instance.dns_name
+  description = "The domain name of the load balancer in the 3rd example"
+}
